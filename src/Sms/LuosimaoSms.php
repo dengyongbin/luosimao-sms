6eb1b8968e9946334afcb91591d3d98ced2963dc
dengyongbin <dengyongbin@loopeer.com>
2015-06-04 11:51:58 +0800

    sms code init

---
+++ src/Sms/LuosimaoSms.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Copyright (C) Loopeer, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential.
+ *
+ * User: DengYongBin
+ * Date: 15/6/4
+ * Time: 上午11:29
+ */
+class LuosimaoSms {
+
+    protected $apiKey;
+
+    public function __construct($apiKey) {
+        $this->apiKey = $apiKey;
+    }
+
+    public function setApiKey($apiKey) {
+        $this->apiKey = $apiKey;
+    }
+
+    /**
+     * @param $phone
+     * @param $msg 验证码：%s，请于30分钟内输入使用 【绿葡科技】
+     * @param $captcha
+     * @return mixed
+     */
+    public function sendSms($phone, $msg, $captcha) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "http://sms-api.luosimao.com/v1/send.json");
+        curl_setopt($ch, CURLOPT_ENCODING ,"UTF-8");
+
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, FALSE);
+
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, $this->apiKey);
+
+        curl_setopt($ch, CURLOPT_POST, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+                'mobile' => $phone,
+                'message' => sprintf($msg, $captcha)
+            )
+        );
+        $res = curl_exec($ch);
+        curl_close($ch);
+        return $res;
+    }
+}
